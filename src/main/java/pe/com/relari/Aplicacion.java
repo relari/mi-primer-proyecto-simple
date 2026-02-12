d4f1ab674939a20190f9bf0e55ef69c9ba5ad19e
Relari <renzolavador@gmail.com>
2026-02-12 14:20:03 -0500

    subiendo mi primer proyecto en maven con java

---
+++ src/main/java/pe/com/relari/Aplicacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pe.com.relari;
+
+/**
+ *
+ * @author Relari
+ */
+public class Aplicacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
