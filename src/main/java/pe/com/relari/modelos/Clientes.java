bcfb6304f17c02725bb37148fb154166d78ef608
Relari <renzolavador@gmail.com>
2026-02-12 14:54:29 -0500

    Creando la clase Cliente que tiene los datos personales

---
+++ src/main/java/pe/com/relari/modelos/Clientes.java
@@ -0,0 +1,71 @@
+package pe.com.relari.modelos;
+
+public class Clientes {
+
+    private Integer idCliente;
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String telefono;
+    private String correo;
+
+    public Clientes() {
+    }
+
+    public Clientes(Integer idCliente, String nombre, String apellido, String dni, String telefono, String correo) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+}
