c54dbcd6da26eace9f9dc157fcfae2a6652d094a
Relari <renzolavador@gmail.com>
2026-02-12 14:55:07 -0500

    APP

--- src/main/java/pe/com/relari/Aplicacion.java
+++ src/main/java/pe/com/relari/Aplicacion.java
@@ -4,6 +4,9 @@
 
 package pe.com.relari;
 
+import pe.com.relari.formularios.LavanderiaFormulario;
+import pe.com.relari.modelos.Clientes;
+
 /**
  *
  * @author Relari
@@ -12,5 +15,8 @@ public class Aplicacion {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        var lavanderiaFormulario = new LavanderiaFormulario();
+        var clientes = new Clientes(1, "Juan", "Perez", "12345678", "987654321", "");
+        lavanderiaFormulario.btnRegistrarCliente(clientes);
     }
 }
